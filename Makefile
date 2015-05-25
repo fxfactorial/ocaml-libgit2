02b4aaf7b1f88d29dd1033ec7b749b9cfe7cb14a
Edgar Aroutiounian <edgar.factorial@gmail.com>
2015-05-25 15:31:00 -0400

    First simple example, getting version of git

--- Makefile
+++ Makefile
@@ -2,9 +2,18 @@ PACKAGES := ctypes.foreign
 CCLIB := libgit2.0.22.0.dylib
 SRC := git.ml
 
-.PHONY:clean
+DEBUG_EXEC := Git_test
 
+.PHONY:clean generate_bindings
+
+generate_bindings:
+	ocamlfind ocamlopt -package $(PACKAGES) -cclib $(CCLIB) \
+	-thread -linkpkg git.ml -o $@
+	./$@
 
 debug:
 	ocamlfind ocamlopt -package $(PACKAGES) -cclib $(CCLIB) \
-	-linkpkg $(SRC) -o Git_test
+	-linkpkg $(SRC) -o $(DEBUG_EXEC)
+
+clean:
+	rm -rf *.cmi *.cmt *.cmx *.cmo *.o $(DEBUG_EXEC) generate_bindings
--- git.ml
+++ git.ml
@@ -1,24 +1,14 @@
 open Ctypes
 open Foreign
 
-let prepare_git =
-  foreign "git_libgit2_init" (ptr void @-> returning void)
-
-type git_repository
-let git_repository : git_repository structure typ = structure "git_repository"
-
-let check_if_repo =
-  foreign
-    "git_repository_open_ext"
-    ((ptr (ptr git_repository)) @-> string @-> int @-> string @-> returning int)
+module Common = struct
+    let git_libgit2_version =
+      foreign "git_libgit2_version" (ptr int @-> ptr int @-> ptr int @-> returning void)
+  end
 
 let () =
-  prepare_git null;
-  let result = check_if_repo
-                 (from_voidp (ptr git_repository) null)
-                 "/Users/Edgar/Repos/oboto"
-                 0
-                 ""
-  in
-  print_string (string_of_int result)
-
+  let major = allocate int 0 in
+  let minor = allocate int 0 in
+  let rev = allocate int 0 in
+  Common.git_libgit2_version major minor rev;
+  Printf.sprintf "%d.%d.%d" (!@ major) (!@ minor) (!@ rev) |> print_endline
